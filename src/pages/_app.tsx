cbbb61fbfb3a0e953ee639eab51d14bea2b2b949
Hichem Neggaz <101028015+HichemNgz@users.noreply.github.com>
2024-06-02 23:28:59 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -122,7 +122,7 @@ function CustomApp({
             n.queue=[];t=b.createElement(e);t.async=!0;
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/all.js');
+            'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', 'TEST24841');
           `,
         }}
