a4ee1154ce3f68543349c2b65b6e1e5e7bb99abc
Hichem Neggaz <101028015+HichemNgz@users.noreply.github.com>
2024-06-02 16:48:54 +0100

    Update FbPixel.tsx

--- src/components/FbPixel.tsx
+++ src/components/FbPixel.tsx
@@ -1,42 +1,45 @@
-'use client'
+'use client';
 
 import { useRouter } from 'next/router';
-import React, { useEffect } from 'react'
-import FacebookPixel from 'react-facebook-pixel';
-
+import React, { useEffect } from 'react';
 
 const FbPixel = () => {
-    const router = useRouter();
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-          console.log('Window is defined');
-          const pixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
-          console.log('Pixel ID:', pixelId);
-          if (pixelId) {
-            FacebookPixel.init(pixelId);
-            FacebookPixel.pageView();
-      
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      console.log('Window is defined');
+      const pixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
+      console.log('Pixel ID:', pixelId);
+
+      if (pixelId) {
+        import('react-facebook-pixel')
+          .then((FacebookPixel) => {
+            FacebookPixel.default.init(pixelId);
+            FacebookPixel.default.pageView();
+
             const handleRouteChange = () => {
               console.log('Route changed, tracking page view');
-              FacebookPixel.pageView();
+              FacebookPixel.default.pageView();
             };
-      
+
             router.events.on('routeChangeComplete', handleRouteChange);
             return () => {
               router.events.off('routeChangeComplete', handleRouteChange);
             };
-          } else {
-            console.error('Facebook Pixel ID is not defined');
-          }
-        } else {
-          console.log('Window is undefined');
-        }
-      }, [router.events]);
-  return (
-    <div>
-      
-    </div>
-  )
-}
+          })
+          .catch((error) => {
+            console.error('Error importing react-facebook-pixel:', error);
+          });
+      } else {
+        console.error('Facebook Pixel ID is not defined');
+      }
+    } else {
+      console.log('Window is undefined');
+    }
+  }, [router.events]);
 
-export default FbPixel
+  return null; // No need to render a div
+};
+
+export default FbPixel;
