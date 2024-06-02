553f2fec8b57a7e7fc0a69ea9799401872b7a8be
Hichem Neggaz <101028015+HichemNgz@users.noreply.github.com>
2024-06-02 22:16:36 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import { ToastContainer } from 'react-toastify';
 // import { ReactQueryDevtools } from 'react-query/devtools';
 import { appWithTranslation } from 'next-i18next';
 import DefaultSeo from '@components/common/default-seo';
+import FacebookPixel from 'react-facebook-pixel';
 
 // Load Open Sans and satisfy typeface font
 import '@fontsource/open-sans';
@@ -36,8 +37,8 @@ import { SessionProvider } from 'next-auth/react';
 import Maintenance from '@components/maintenance/layout';
 import '../assets/styles/animation.css';
 
-
-
+import Script from 'next/script';
+import * as fbq from '../lib/fpixel';
 
 function handleExitComplete() {
   if (typeof window !== 'undefined') {
@@ -83,40 +84,69 @@ function CustomApp({
   useEffect(() => {
     document.documentElement.dir = dir;
   }, [dir]);
-  
 
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview();
 
+    const handleRouteChange = () => {
+      fbq.pageview();
+    };
 
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
-    <AnimatePresence initial={false} onExitComplete={handleExitComplete}>
-      <SessionProvider session={session}>
-        <QueryClientProvider client={queryClient}>
-          <Hydrate state={pageProps.dehydratedState}>
-            <AppSettings>
-              <ManagedUIContext>
-                <DefaultSeo />
-                <Maintenance>
-                 
-                  {Boolean(authProps) ? (
-                    <PrivateRoute>
-                      {getLayout(<Component {...pageProps} />)}
-                    </PrivateRoute>
-                  ) : (
-                    getLayout(<Component {...pageProps} />)
-                  )}
-                </Maintenance>
-                <ToastContainer autoClose={2000} theme="colored" />
-                <SocialLoginProvider />
-                <ManagedModal />
-                <ManagedDrawer />
-              </ManagedUIContext>
-            </AppSettings>
-          </Hydrate>
-          {/* <ReactQueryDevtools /> */}
-        </QueryClientProvider>
-      </SessionProvider>
-    </AnimatePresence>
+    <>
+      
+      <AnimatePresence initial={false} onExitComplete={handleExitComplete}>
+        <SessionProvider session={session}>
+          <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydratedState}>
+              <AppSettings>
+                <ManagedUIContext>
+                  <DefaultSeo />
+                  <Maintenance>
+                  <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
+                    {Boolean(authProps) ? (
+                      <PrivateRoute>
+                        {getLayout(<Component {...pageProps} />)}
+                      </PrivateRoute>
+                    ) : (
+                      getLayout(<Component {...pageProps} />)
+                    )}
+                  </Maintenance>
+                  <ToastContainer autoClose={2000} theme="colored" />
+                  <SocialLoginProvider />
+                  <ManagedModal />
+                  <ManagedDrawer />
+                </ManagedUIContext>
+              </AppSettings>
+            </Hydrate>
+            {/* <ReactQueryDevtools /> */}
+          </QueryClientProvider>
+        </SessionProvider>
+      </AnimatePresence>
+    </>
   );
 }
 
